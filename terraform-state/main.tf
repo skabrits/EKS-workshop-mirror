b11ab20c24dee5c855d70f4d8f9551aac4ea2e7b
sevak <seva.kabrits@gmail.com>
2023-06-21 15:34:41 +0300

    fix

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -10,6 +10,7 @@ resource "aws_s3_bucket" "lbuck" {
 
 resource "aws_dynamodb_table" "ldb" {
   name = var.lock_table
+  billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
